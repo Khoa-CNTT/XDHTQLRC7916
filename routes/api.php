12f4952bdbc4bfd585c4aa14e457358f7710ceef
Quang hùng Nguyễn <quanghung632002@gmail.com>
2025-04-12 13:25:18 +0700

    the loai

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,22 @@
 <?php
 
+use App\Http\Controllers\TheLoaiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::group(['prefix' => '/admin'], function () {
+
+    Route::group(['prefix' => '/the-loai-phim'], function () {
+        Route::get('/lay-du-lieu', [TheLoaiController::class, 'getData']);
+        Route::post('/search', [TheLoaiController::class, 'searchTheLoai']);
+        Route::post('/create', [TheLoaiController::class, 'createTheLoai']);
+        Route::delete('/delete/{id}', [TheLoaiController::class, 'deleteTheLoai']);
+        Route::put('/update', [TheLoaiController::class, 'updateTheLoai']);
+    });
+});
+
